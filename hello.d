80873211b818771cd641b5b6ee90cf50255afb89
John W. Long <me@johnwlong.com>
2016-11-03 21:25:38 -0400

    Allow for passing parameters

--- hello.d
+++ hello.d
@@ -1,6 +1,14 @@
 import std.stdio;
 
-void main()
-{
-  writeln("Hello world!");
+int main(string[] args) {
+  int code = 0;
+  if (args.length == 1) {
+    writeln("Hello world!");
+  } else if (args.length == 2) {
+    writefln("Hello %s!", args[1]);
+  } else {
+    writeln("hello: too many parameters\n");
+    code = 1;
+  }
+  return code;
 }
